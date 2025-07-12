9b01fdc3e8900d4917ea1b500f9dacb75883042d
Ignatius47 <ignatiusx47@gmail.com>
2025-07-12 22:14:06 +0300

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,18 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
+      <WelcomeMessage />
+
+      <Header />
+      <MainContent />
+      <Footer />
+
       <UserProfile
         name="Alice"
         age="25"
