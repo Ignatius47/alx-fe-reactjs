e3d12ee4989fea2db9c9ee27679858b5e0a594d8
Ignatius47 <ignatiusx47@gmail.com>
2025-07-31 11:16:41 +0300

    Initial Commit

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import { useRecipeStore } from '../stores/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ borderBottom: '1px solid #ccc', marginBottom: '1rem' }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
