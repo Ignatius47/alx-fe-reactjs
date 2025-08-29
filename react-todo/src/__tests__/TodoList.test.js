6e77d5bdff8880ea9f471eeb78a113325f2d910f
Ignatius <ignatiusx47@gmail.com>
2025-08-29 12:59:46 +0300

    Todo List Functionality

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build Todo App")).toBeInTheDocument();
+  });
+
+  test("can add a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText("Add new todo");
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.click(screen.getByText("Add"));
+    expect(screen.getByText("New Task")).toBeInTheDocument();
+  });
+
+  test("can toggle todo completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("can delete a todo", () => {
+    render(<TodoList />);
+    const deleteButton = screen.getByText("Build Todo App").nextSibling;
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText("Build Todo App")).not.toBeInTheDocument();
+  });
+});
